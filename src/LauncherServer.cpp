b256e592eb22cc115c91ddbe5774cfd78ef21c1c
p2vman <andreywontomsky@gmail.com>
2025-04-27 01:27:56 +0300

    Update LauncherServer.cpp

--- src/LauncherServer.cpp
+++ src/LauncherServer.cpp
@@ -79,7 +79,8 @@ void async_accept(tcp::acceptor &acceptor, boost::asio::io_context &io_context) 
 void main_thread() {
     try {
         boost::asio::io_context io_context;
-        tcp::acceptor acceptor(io_context, {tcp::v4(), 8080});
+        tcp::acceptor acceptor(io_context, {tcp::v4(), std::stoi(std::getenv("PORT") ? std::getenv("PORT") : "8080")});
+
 
         async_accept(acceptor, io_context);
         io_context.run();
